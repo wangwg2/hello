c2c826872ce5a4d3a60114fc8f2469c56bc7ce17
wangwg2 <wangwg2@msn.com>
2023-04-08 16:14:38 +0800

    using stingutil

--- README.md
+++ README.md
@@ -11,3 +11,19 @@ go run hellogo.go
 # output
 Hello, Welcome to Go
 ```
+
+## go workspace
+
+在 `hellogo` 同级目录 创建 `stringutil`
+
+修改 `hellogo.go` 使用 `stringutil`
+
+
+在 workspace 目录(`demows`) 运行
+```bash
+# 设置
+go work init ./hellogo
+go work use ./stringutil
+# 运行 hellogo
+go run github.com/wangwg2/hellogo
+```
--- hellogo.go
+++ hellogo.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/wangwg2/stringutil"
 )
 
 func main() {
-	fmt.Printf("Hello, Welcome to Go!\n")
+	fmt.Printf("github.com/wangwg2/hellogo: import - github.com/wangwg2/stringutil\n")
+	fmt.Printf(stringutil.Reverse("\n!oG ,olleH"))
+	fmt.Printf(stringutil.Reverse("\nHello, Welcome to Go!"))
 }
